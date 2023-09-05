f7f5d6fb182798d0e7f23d995916e0ffae34c869
Carlos <carlosjcv94@gmail.com>
2023-09-05 22:05:30 +0200

    Testing error handler

--- app.js
+++ app.js
@@ -2,76 +2,83 @@ const express = require("express"); // Starts our connection with Express
 const app = express(); // Starts our connection with Express
 const path = require("path");
 const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate")
-const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 // We start our connection to our Mongo DB through mongoose
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("DB connected!")
-})
+    console.log("DB connected!");
+});
 
 // Makes our connection to EJS, so we can show those views
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"));
 
 // Tell express to parse the request body from our POST requests
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 // Render from our first Home page
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
-app.get("/campgrounds", async(req, res) => {
+app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds })
-})
+    res.render("campgrounds/index", { campgrounds });
+});
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("campgrounds/new")
-})
+    res.render("campgrounds/new");
+});
 
 app.post("/campgrounds", async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
-app.get("/campgrounds/:id", async(req, res) => {
+app.get("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render("campgrounds/show", { campground });
-})
+});
 
-app.get("/campgrounds/:id/edit", async(req, res) => {
+app.get("/campgrounds/:id/edit", async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render("campgrounds/edit", { campground });
-})
+});
 
-app.put("/campgrounds/:id", async(req, res) => {
+app.put("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}) // First argument ID, and second argument we need to pass through the objects title location, in a dinamic way, so instead of hard coding it, we need to take what's in req.body.campground (we are grouping things into campground) and spread the object, that's why ...req.body.campground
+    const campground = await Campground.findByIdAndUpdate(id, {
+        ...req.body.campground,
+    }); // First argument ID, and second argument we need to pass through the objects title location, in a dinamic way, so instead of hard coding it, we need to take what's in req.body.campground (we are grouping things into campground) and spread the object, that's why ...req.body.campground
     res.redirect(`/campgrounds/${campground._id}`);
-})
+});
 
-app.delete("/campgrounds/:id", async(req, res) => {
+app.delete("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
-})
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something went wrong :(" } = err;
+    res.status(status).send(message);
+});
 
 // Starts our connection with Express
 app.listen(3000, () => {
-    console.log("Serving on port 3000")
-})
+    console.log("Serving on port 3000");
+});
