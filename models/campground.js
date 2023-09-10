ba964a3db1cf574a09a069bd21bdfdf992c58a9c
Carlos <carlosjcv94@gmail.com>
2023-09-10 20:03:39 +0200

    Reviews deleted with campground implementation

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -14,6 +15,17 @@ const CampgroundSchema = new Schema({
     }
   ]
 });
+
+// This mongoose Middleware is to have access to the object that has been deleted, that way we can access references, for example, any review that might've been done on a camp, we can then use the IDs for those reviews, on the deleted camp, to also delete the reviews
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
+})
 
 // Compiling our model to export it
 module.exports = mongoose.model("Campground", CampgroundSchema);
