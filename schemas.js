19801095ecf07485bf6821b30471c597cee50a4b
Carlos <carlosjcv94@gmail.com>
2023-09-10 16:42:14 +0200

    Added JOI review schema

---
+++ schemas.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+    }).required()
+})
+
+// First require and then min, max, other way around, the rating does not go through validation, because there will always be missing data
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
