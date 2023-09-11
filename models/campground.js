e09190e46619e1e8f0752d3463d6fcb52fee0e73
Carlos <carlosjcv94@gmail.com>
2023-09-11 23:52:42 +0200

    Mongoose middleware to delete review with Campground delete

--- models/campground.js
+++ models/campground.js
@@ -3,28 +3,28 @@ const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  title: String,
-  image: String,
-  price: Number,
-  description: String,
-  location: String,
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Review"
-    }
-  ]
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 // This mongoose Middleware is to have access to the object that has been deleted, that way we can access references, for example, any review that might've been done on a camp, we can then use the IDs for those reviews, on the deleted camp, to also delete the reviews
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
-  if (doc) {
-    await Review.deleteMany({
-      _id: {
-        $in: doc.reviews
-      }
-    })
-  }
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 // Compiling our model to export it
