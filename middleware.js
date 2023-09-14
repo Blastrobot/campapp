cadb2d3e95eb0777172169d77376edaf8f79c94d
Carlos <carlosjcv94@gmail.com>
2023-09-14 16:29:54 +0200

    Fixed register route to keep the logged in credentials and middleware to return to where the user was added

--- middleware.js
+++ middleware.js
@@ -1,8 +1,17 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    // console.log("req.user is this:", req.user); // this is there by passport, it is going to be automatically filled in with the deserialized information from the session, so the session store is serialized using passport
     if(!req.isAuthenticated()){ // this method is coming from passport
+        req.session.returnTo = req.originalUrl;
         req.flash("error", "You must be logged in first! 🙄");
         res.redirect("/login");
     } else {
         next();
     }
+}
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
 }
--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,25 @@ const router = express.Router();
 const passport = require("passport")
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
+const { storeReturnTo } = require("../middleware");
 
 router.get("/register", (req, res) => {
     res.render("users/register");
 });
 
-router.post("/register", catchAsync(async (req, res) => {
+router.post("/register", catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash("success", "Welcome to BlastCamp! 🥰");
-        res.redirect("/campgrounds");
+        req.login(registeredUser, err => {
+            if (err) {
+                next(err)
+            } else {
+                req.flash("success", "Welcome to BlastCamp! 🥰");
+                res.redirect("/campgrounds");
+            }
+        })
     } catch (e) {
         req.flash("error", e.message); // the catched error will contain a message, this is just coming from my passport implementation
         res.redirect("/register");
@@ -25,9 +32,21 @@ router.get("/login", (req, res) => {
     res.render("users/login");
 });
 
-router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+router.post("/login", storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
     req.flash("success", "Welcome back! 🙌🏾");
-    res.redirect("/campgrounds");
+    const redirectUrl = res.locals.returnTo || "/campgrounds";
+    res.redirect(redirectUrl);
+})
+
+router.get("/logout", (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            next(err);
+        } else {
+            req.flash("success", "See you soon! 👋🏽")
+            res.redirect("/campgrounds");
+        }
+    });
 })
 
 module.exports = router;
