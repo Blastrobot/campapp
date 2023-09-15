ec8b430d92649237adf87e21e55d4a869ce4cc56
Carlos <carlosjcv94@gmail.com>
2023-09-15 14:43:58 +0200

    Campgrounds and Users routes were fancier restructured with router.route, implemented Multer Middleware Upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,19 +3,25 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds.js");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
-router.get("/", catchAsync(campgrounds.index));
+router.route("/")
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, upload.array("image"), catchAsync(campgrounds.createCampground));
+    // .post(upload.array("image"), (req, res) => {
+    //     console.log(req.body, req.files)
+    //     res.send("IT WORKED?!");
+    // })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router.route("/:id")
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
