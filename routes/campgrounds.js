05333b5538ab99fcb50bdd9637095a154f0598d6
Carlos <carlosjcv94@gmail.com>
2023-09-15 10:42:41 +0200

    Routes were restructured using controllers, and added middleware to check review authorization on Reviews

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,61 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const campgrounds = require("../controllers/campgrounds.js");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
-const Campground = require("../models/campground");
 
-router.get("/", catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
-}));
+router.get("/", catchAsync(campgrounds.index));
 
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("campgrounds/new");
-});
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res) => {
-    // if (!req.body.campground) throw new ExpressError("Invalid campground data!", 400) // Even tho we got a validation on the client side, there's the possibility to do a POST method, through Postman for example, and that will still make an empty campground, that's why we need this line
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash("success", "New campground was successfuly made! 🥳")
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get("/:id", catchAsync(async (req, res) => {
-    // Instead of just destructuring with id of req.params, I use _id that way I also can throw a flash for an error when Mongoose cannot cast an ObjectId, basically because Mongo works with _id instead of id, both works, but if I type for example extra characters for that automated id that Mongo creates, then id stops working and I don't get a flash error
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate("reviews").populate("author");
-    // console.log(campground)
-    if(!campground){
-        req.flash("error", "Campground was not found! 😔");
-        return res.redirect("/campgrounds");
-    }
-    res.render("campgrounds/show", { campground });
-}));
+router.get("/:id", catchAsync(campgrounds.showCampground));
 
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if(!campground){
-        req.flash("error", "Campground was not found! 😔");
-        return res.redirect("/campgrounds");
-    }
-    res.render("campgrounds/edit", { campground });
-}));
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground,}); // First argument ID, and second argument we need to pass through the objects title location, in a dinamic way, so instead of hard coding it, we need to take what's in req.body.campground (we are grouping things into campground) and spread the object, that's why ...req.body.campground
-    req.flash("success", "Campground was successfully updated! 🥳")
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash("success", "Campground was successfully deleted! 🤯")
-    res.redirect("/campgrounds");
-}));
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,11 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-
+const reviews = require("../controllers/reviews.js");
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
-const Review = require("../models/review");
+const { isLoggedIn, validateReview, isReviewAuthor } = require("../middleware");
 
-const { isLoggedIn, validateReview } = require("../middleware");
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.post("/", isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", "New review was successfully made! 🥳")
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-router.delete("/:reviewId", catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review was successfully deleted! 🤯")
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
