f61bfffcf1dc146d16c52476a705b8f13360f2eb
Carlos <carlosjcv94@gmail.com>
2023-09-18 22:23:51 +0200

    Virtual method added to give accessibility to the properties in the object JSON of a campground for the Popup

--- models/campground.js
+++ models/campground.js
@@ -9,7 +9,9 @@ const ImageSchema = new Schema({
 
 ImageSchema.virtual("thumbnail").get(function() {
     return this.url.replace("/upload", "/upload/w_200");
-})
+});
+
+const opts = { toJSON: { virtuals: true }}; // Virtuals are normally not inside the response in the JSON include, that's why this is needed to include that data in the JSON, making it accessible
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -37,8 +39,14 @@ const CampgroundSchema = new Schema({
             type: [Number],
             required: true
         }
-    }
-});
+    },
+}, opts);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function() {
+    return `
+    <a href="/campgrounds/${this._id}">${this.title}</a>
+    <p>${this.description.substring(0, 30)}...</p>`; // this refers to a particular campground instance
+})
 
 // This mongoose Middleware is to have access to the object that has been deleted, that way we can access references, for example, any review that might've been done on a camp, we can then use the IDs for those reviews, on the deleted camp, to also delete the reviews
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
